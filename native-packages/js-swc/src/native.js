a621820c2acdd3cd3796c5a135f6e1c451bc42d1
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-04-12 21:02:41 +0200

    swc transformer wasm

--- native-packages/js-swc/src/native.js
+++ native-packages/js-swc/src/native.js
@@ -12,4 +12,22 @@ if (process.platform === 'linux') {
   parts.push('msvc');
 }
 
-module.exports = require(`../parcel-swc.${parts.join('-')}.node`);
+module.exports = process.env.SWC_WASM
+  ? (() => {
+      let wasm = require('../wasm/pkg/');
+      return {
+        transform: (...args) => {
+          let result = wasm.transform(...args);
+          return {
+            ...result,
+            hoist_result: {
+              ...result.hoist_result,
+              exported_symbols: Object.fromEntries([
+                ...result.hoist_result.exported_symbols,
+              ]),
+            },
+          };
+        },
+      };
+    })()
+  : require(`../parcel-swc.${parts.join('-')}.node`);
--- native-packages/js-swc/wasm/src/lib.rs
+++ native-packages/js-swc/wasm/src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use wasm_bindgen::prelude::*;
+
+use serde_wasm_bindgen;
+use parcel_js_swc_core::{transform as coreTransform, Config};
+
+#[wasm_bindgen]
+pub fn transform(val: JsValue) ->  Result<JsValue, JsValue> {
+  let config: Config = serde_wasm_bindgen::from_value(val)?;
+
+  let result = coreTransform(config).unwrap();
+  serde_wasm_bindgen::to_value(&result).map_err(|err| err.into())
 }
